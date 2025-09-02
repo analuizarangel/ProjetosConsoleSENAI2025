297237ab9f6b7791680916aac30a04b77782f2fe
analuizarangel <aanaluiza645@gmail.com>
2025-09-02 11:38:12 -0300

    Add files via upload

---
+++ Exercício 04/Exercício 04/Exercício 04.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Digite sua idade em anos.");
+int anos = int.Parse(Console.ReadLine());
+Console.WriteLine("Digite sua idade em meses.");
+int meses = int.Parse(Console.ReadLine());
+Console.WriteLine("Digite sua idade em dias.");
+int dias = int.Parse(Console.ReadLine());
+int IdadeEmDias = (anos * 365) + (meses * 30) + dias;
+Console.WriteLine($"A sua idade em dias é {IdadeEmDias}");
