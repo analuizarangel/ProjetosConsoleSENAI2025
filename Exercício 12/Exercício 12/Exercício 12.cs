f18c8635cfe8ec9b489529079f38dbed07f36d0e
analuizarangel <aanaluiza645@gmail.com>
2025-09-09 11:10:28 -0300

    Add files via upload

---
+++ Exercício 12/Exercício 12/Exercício 12.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Digite o primeiro valor: ");
+int valor1 = int.Parse(Console.ReadLine());
+
+Console.Write("Digite o segundo valor: ");
+int valor2 = int.Parse(Console.ReadLine());
+
+if (valor1 > valor2)
+{
+    Console.WriteLine($"O maior valor é: {valor1}");
+}
+else
+{
+    Console.WriteLine($"O maior valor é: {valor2}");
+}
