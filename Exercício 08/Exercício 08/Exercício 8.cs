a33d8ae467337448ce9d607cf5f1bca772f7acec
analuizarangel <aanaluiza645@gmail.com>
2025-09-09 07:58:44 -0300

    Add files via upload

---
+++ Exercício 08/Exercício 08/Exercício 8.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Digite um valor.");
+int valor = int.Parse(Console.ReadLine());
+if (valor > 0)
+{
+    Console.WriteLine("é um valor positivo.");
+}
+else if (valor < 0)
+{
+    Console.WriteLine("é um valor negativo.");
+}
+else
+{
+    Console.WriteLine("Valores iguais a 0 não são permitidos.");
+}
