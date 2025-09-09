87d2bead8c7a41e9cb8fd5194a63b72f6e978f95
analuizarangel <aanaluiza645@gmail.com>
2025-09-09 11:08:12 -0300

    Add files via upload

---
+++ Exercício 11/Exercício 11/Exercício 11.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Digite o ano atual: ");
+int anoAtual = int.Parse(Console.ReadLine());
+
+Console.Write("Digite o ano de nascimento: ");
+int anoNascimento = int.Parse(Console.ReadLine());
+
+int idade = anoAtual - anoNascimento;
+
+if (idade < 16)
+{
+    Console.WriteLine("Não vota.");
+}
+else if ((idade >= 16 && idade < 18) || idade > 70)
+{
+    Console.WriteLine("Voto facultativo.");
+}
+else
+{
+    Console.WriteLine("Voto obrigatório.");
+}
