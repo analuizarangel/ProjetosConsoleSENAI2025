7b13690cb1cbe0fa06c884d17c5c6820e2fee41d
analuizarangel <aanaluiza645@gmail.com>
2025-09-09 10:22:14 -0300

    Add files via upload

---
+++ Exercício 10/Exercício 10/exercício 10.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Digite a nota da 1ª avaliação\n");
+double nota1 = double.Parse(Console.ReadLine());
+
+Console.Write("Digite a nota da 2ª avaliação\n");
+double nota2 = double.Parse(Console.ReadLine());
+
+double media = (nota1 + nota2) / 2;
+
+Console.WriteLine($"Média do aluno: {media:F1}");
+
+if (media >= 6)
+{
+    Console.WriteLine("Aluno aprovado!");
+}
+else
+{
+    Console.WriteLine("Aluno reprovado!");
+}
